851ed17f72b6f66d097c561fa35babc9a3e9e290
Mostafa Younis <darroosh2@gmail.com>
2025-02-01 15:38:16 +0200

    Preparing 2

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
